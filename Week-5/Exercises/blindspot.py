2a180d9f79875135aa01e746cdc9aefe9b25db69
MengtingJ <mengting.j@outlook.com>
2025-10-11 17:33:44 +0200

    Save Week-5 Answer

--- Week-5/Exercises/blindspot.py
+++ Week-5/Exercises/blindspot.py
@@ -8,7 +8,8 @@ from expyriment.misc.constants import K_LEFT, K_RIGHT, K_UP, K_DOWN, K_1, K_2, K
 exp = design.Experiment(name="Blindspot", background_colour=C_WHITE, foreground_colour=C_BLACK)
 control.set_develop_mode()
 control.initialize(exp)
-# exp.keyboard.set_key_repeat(True)
+
+exp.add_data_variable_names(['eye', 'key', 'final_radius', 'final_x', 'final_y'])
 
 """ Stimuli """
 def make_circle(r, pos=(0,0)):
@@ -39,18 +40,28 @@ def run_trial(side = 'left'):
     text_screen.present()
     exp.keyboard.wait([K_SPACE]) 
 
-    cross_pos = [300, 0] if side.lower() == "left" else [-300, 0]
+    cross_pos = [0, 0]
     fixation = stimuli.FixCross(size=(150, 150), line_width=10, position=cross_pos)
     fixation.preload()
 
     radius = 50
-    pos = [0, 0]
-    circle = make_circle(radius)
+    pos = [-300, 0] if side.lower() == "left" else [300, 0]
+    circle = make_circle(radius, pos = pos)
 
     # the change in position for each press
     d_pos = 15
     # the change in size for each press
     d_size = 5
+
+    key_map = {
+        K_LEFT: 'LEFT',
+        K_RIGHT: 'RIGHT',
+        K_UP: 'UP',
+        K_DOWN: 'DOWN',
+        K_1: 'larger',
+        K_2: 'smaller',
+        K_SPACE: 'quit'
+    }
 
     while True:
         exp.screen.clear()
@@ -59,29 +70,33 @@ def run_trial(side = 'left'):
 
         key = exp.keyboard.check([K_LEFT, K_RIGHT, K_UP, K_DOWN, K_1, K_2, K_SPACE])
         # print("Pressed:", key)
+        
+        if key:
+            exp.data.add([side.upper(), key_map.get(key, str(key)), radius, pos[0], pos[1]])
+            # --- Movement ---
+            if key == K_LEFT:
+                pos[0] -= d_pos
+            elif key == K_RIGHT:
+                pos[0] += d_pos
+            elif key == K_UP:
+                pos[1] += d_pos
+            elif key == K_DOWN:
+                pos[1] -= d_pos
 
-        # --- Movement ---
-        if key == K_LEFT:
-            pos[0] -= d_pos
-        elif key == K_RIGHT:
-            pos[0] += d_pos
-        elif key == K_UP:
-            pos[1] += d_pos
-        elif key == K_DOWN:
-            pos[1] -= d_pos
+            # --- Size adjustment ---
+            elif key == K_1:
+                radius = max(d_size, radius - d_size)
+            elif key == K_2:
+                radius += d_size
 
-        # --- Size adjustment ---
-        elif key == K_1:
-            radius = max(d_size, radius - d_size)
-        elif key == K_2:
-            radius += d_size
+            # --- Update circle ---
+            circle = make_circle(radius, pos)
 
-        # --- Update circle ---
-        circle = make_circle(radius, pos)
-
-        # --- Finish trial ---
-        if key == K_SPACE:
-            break
+            # --- Finish trial ---
+            if key == K_SPACE:
+                # record data
+                print("CSV file will be saved in:", exp.data.fullpath)
+                break
 
 control.start(subject_id=random.randint(1, 999))
 
@@ -89,3 +104,15 @@ run_trial(side = 'left')
 run_trial(side = 'right')
     
 control.end()
+
+
+# ---------------------------------------
+# print collected data
+# ---------------------------------------
+from expyriment.misc.data_preprocessing import read_datafile
+import pandas as pd
+
+# read .xpd 
+data, variables, subject_info, comments = read_datafile(f"data/blindspot_{exp.subject}.xpd")
+df = pd.DataFrame(data, columns=variables)
+print(df)
