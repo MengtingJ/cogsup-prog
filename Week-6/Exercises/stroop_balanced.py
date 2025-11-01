ce706e366ac7b569c24080dbf1d7dbee27917abc
MengtingJ <mengting.j@outlook.com>
2025-11-01 18:20:28 +0100

    Save Week-6 Answer

--- Week-6/Exercises/stroop_balanced.py
+++ Week-6/Exercises/stroop_balanced.py
@@ -1,9 +1,7 @@
 from expyriment import design, control, stimuli
-from expyriment.misc.constants import C_WHITE, C_BLACK, K_j, K_f
+from expyriment.misc.constants import C_WHITE, C_BLACK, K_r, K_b, K_g, K_o
 import random
 import itertools
-
-
 
 # Helper for obtaining derangements in python
 def derangements(lst):
@@ -20,7 +18,13 @@ def derangements(lst):
 
 
 """ Constants """
-KEYS = [K_j, K_f]
+COLOR_KEYS = {
+    K_r: 'red',
+    K_b: 'blue',
+    K_g: 'green',
+    K_o: 'orange'
+}
+KEYS = [K_r, K_b, K_g, K_o]
 TRIAL_TYPES = ["match", "mismatch"]
 WORDS = ["red", "blue", "green", "orange"]
 COLORS_RGB = {
@@ -30,12 +34,13 @@ COLORS_RGB = {
     "orange": (255, 165, 0)
 }
 
-N_BLOCKS = 2
+
+N_BLOCKS = 8
 N_TRIALS_IN_BLOCK = 16
 
 INSTR_START = """
-In this task, you have to indicate whether the meaning of a word and the color of its font match.
-Press J if they do, F if they don't.\n
+In this task, you have to indicate what is the color of word's font.
+Press R for red, B for blue, G for green, and O for orange\n
 Press SPACE to continue.
 """
 INSTR_MID = """You have finished half of the experiment, well done! Your task will be the same.\nTake a break then press SPACE to move on to the second half."""
@@ -77,8 +82,8 @@ def present_instructions(text):
     exp.keyboard.wait()
 
 """ Global settings """
-exp = design.Experiment(name="Balanced Stroop with BWS Factor", background_colour=C_WHITE, foreground_colour=C_BLACK)
-exp.add_data_variable_names(['block_cnt', 'trial_cnt', 'trial_type', 'word', 'color', 'RT', 'correct'])
+exp = design.Experiment(name="Balanced Stroop (Color Naming)", background_colour=C_WHITE, foreground_colour=C_BLACK)
+exp.add_data_variable_names(['block_cnt', 'trial_cnt', 'trial_type', 'word', 'color', 'RT', 'key', 'correct'])
 exp.add_bws_factor("assignment", derangements(COLORS))
 
 control.set_develop_mode()
@@ -106,8 +111,8 @@ def run_trial(block_id, trial_id, trial_type, word, color):
     present_for(fixation, t=500)
     stim.present()
     key, rt = exp.keyboard.wait(KEYS)
-    correct = key == K_j if trial_type == "match" else key == K_f
-    exp.data.add([block_id, trial_id, trial_type, word, color, rt, correct])
+    correct = COLOR_KEYS[key] == color
+    exp.data.add([block_id, trial_id, trial_type, word, color, rt, COLOR_KEYS[key], correct])
     feedback = feedback_correct if correct else feedback_incorrect
     present_for(feedback, t=1000)
 
