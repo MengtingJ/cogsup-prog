b069cd987f38f415f3017198d0a237394e4ccba5
MengtingJ <mengting.j@outlook.com>
2025-12-16 21:18:05 +0100

    Update Week-12 Answer

--- Week-12/Exercises/Exercise.R
+++ Week-12/Exercises/Exercise.R
@@ -1,4 +1,5 @@
 # SET WORKING DIRECTORY FIRST
+library(tidyverse)
 full_data <- read_csv("data/148338_220209_095045_M057814.csv")
 print(full_data)
 head(full_data, n=6)
@@ -137,32 +138,37 @@ tidy_data %>%
 # Does SNARC-like effect depend on trial type?
 anova_result <- aov(rt ~ correct_side * trial_type, data = tidy_data)
 summary(anova_result)
+# YES!
 
 # ===== Exercise 5 =====
 # Load all CSVs and tidy dataset
-raw_data <- list.files(path = 'data', pattern = ".csv$", full.names = TRUE) %>%
-  map_dfr(read_csv, col_types = cols(), skip = 2, .id = 'file_id')
+raw_data <- list.files(path = 'data', pattern = ".csv$", full.names = TRUE) %>% #
+  map_dfr(read_csv, col_types = cols(), skip = 2, .id = 'id')
 
 tidy_full_data <- raw_data %>%
-  group_by(file_id) %>%
-  mutate(education = first(response)) %>%
-  ungroup() %>%
-  filter(trialType != "form") %>%
+  group_by(id) %>%                             
+  mutate(education = first(response)) %>%    
+  filter(trialType != "form") %>%         
+  select(where(~ n_distinct(.x) > 1)) %>%  
   select(-c(stimPos_actual, ITI_ms, ITI_f, ITI_fDuration, rowNo)) %>%
-  mutate(trial_number = row_number())
+  mutate(trial_number = row_number()) %>%      
+  ungroup()
+
 
 # ===== Exercise 6 =====
 # Exclude trials with RT <200ms or >1500ms
 filtered_trials <- tidy_full_data %>%
-  filter(rt >= 200, rt <= 1500)
-cat("Excluded trials:", nrow(tidy_full_data) - nrow(filtered_trials), "\n")
-cat("Proportion excluded:", (nrow(tidy_full_data) - nrow(filtered_trials))/nrow(tidy_full_data), "\n")
+  filter(RT >= 200, RT <= 1500)
+n_excluded <- nrow(tidy_full_data) - nrow(filtered_trials)
+prop_excluded <- n_excluded / nrow(tidy_full_data)
+cat("Excluded trials:", n_excluded, "\n")
+cat("Proportion excluded:", round(prop_excluded, 3), "\n")
 
 # ===== Exercise 7 =====
 # Exclude participants with <93% accuracy
 participant_acc <- filtered_trials %>%
   group_by(id) %>%
-  summarize(mean_acc = mean(correct_response))
+  summarize(mean_acc = mean(correct))
 good_ids <- participant_acc %>%
   filter(mean_acc >= 0.93) %>%
   pull(id)
@@ -172,49 +178,83 @@ cat("Proportion excluded:", (n_distinct(filtered_trials$id) - length(good_ids))/
 
 # ===== Exercise 8 =====
 # Summarize RT and accuracy by trial type
-filtered_id %>%
-  group_by(trial_type) %>%
+stroop_summary <- filtered_id %>%
+  group_by(trialType) %>%
   summarize(
-    mean_rt = mean(rt),
-    mean_accuracy = mean(correct_response)
+    mean_rt = mean(RT, na.rm = TRUE),
+    mean_accuracy = mean(correct, na.rm = TRUE),
+    .groups = "drop"
   )
+stroop_summary
+
+rt_wide <- filtered_id %>%
+  group_by(id, trialType) %>%
+  summarize(mean_rt = mean(RT), .groups = 'drop') %>%
+  pivot_wider(names_from = trialType, values_from = mean_rt)
+
+rt_diff <- rt_wide$congruent - rt_wide$incongruent
+
+shapiro_p <- shapiro.test(rt_diff)$p.value
+
+if (shapiro_p > 0.05) {
+  test_result <- t.test(rt_wide$congruent, rt_wide$incongruent, paired = TRUE, alternative = "less")
+  test_type <- "Paired t-test"
+} else {
+  test_result <- wilcox.test(rt_wide$congruent, rt_wide$incongruent, paired = TRUE, alternative = "less")
+  test_type <- "Wilcoxon signed-rank test"
+}
+
+cat("Test used:", test_type, "\n")
+print(test_result)
+
+# YES, there's a Stroop effect.
 
 # ===== Exercise 9 =====
 # Stroop effect (incongruent - congruent) per participant
 stroop_effect <- filtered_id %>%
-  group_by(id, trial_type) %>%
-  summarize(mean_rt = mean(rt), .groups = 'drop') %>%
-  pivot_wider(names_from = trial_type, values_from = mean_rt) %>%
-  mutate(stroop_rt = incongruent - congruent)
+  group_by(id, trialType) %>%
+  summarize(rt = mean(RT), .groups = "drop") %>%
+  pivot_wider(names_from = trialType, values_from = rt) %>%
+  mutate(stroop_rt = incongruent - congruent) 
 stroop_effect
 
 # ===== Exercise 10 =====
 # Plot histogram of stroop_rt
-hist(stroop_effect$stroop_rt, main = "Stroop RT Distribution", xlab = "RT Difference (incongruent - congruent)")
+stroop_rt_vector <- stroop_effect %>% pull(stroop_rt)
+
+hist(stroop_rt_vector,
+     main = "Histogram of Stroop RT Effect",
+     xlab = "Stroop RT (incongruent - congruent)",
+     col = "skyblue",
+     border = "white")
+
+full_stroop <- filtered_id %>%
+  group_by(id, trialType) %>%
+  summarize(error = mean(1 - correct), rt = mean(RT), .groups = 'drop') %>%
+  pivot_wider(names_from = trialType, values_from = c(rt, error))
+full_stroop
 
 # ===== Exercise 11 =====
 # Pivot longer: effect and stroop_value
-full_stroop <- filtered_id %>%
-  group_by(id, trial_type) %>%
-  summarize(
-    rt = mean(rt),
-    error = mean(1 - correct_response),
-    .groups = 'drop'
-  ) %>%
-  pivot_wider(names_from = trial_type, values_from = c(rt, error)) %>%
+full_stroop <- full_stroop %>%
   mutate(
     stroop_rt = rt_incongruent - rt_congruent,
     stroop_error = error_incongruent - error_congruent
-  )
+  ) %>%
+  select(id, stroop_rt, stroop_error)
 
-long_stroop <- full_stroop %>%
-  pivot_longer(cols = c(stroop_rt, stroop_error),
-               names_to = "effect",
-               values_to = "stroop_value")
-long_stroop
+stroop_long <- full_stroop %>%
+  pivot_longer(
+    cols = c(stroop_rt, stroop_error),
+    names_to = "effect",
+    values_to = "stroop_value"
+  )
+stroop_long
 
 # ===== Exercise 12 =====
 # Plot correlation between stroop_rt and stroop_error
 plot(full_stroop$stroop_rt, full_stroop$stroop_error,
      xlab = "Stroop RT", ylab = "Stroop Error", main = "Correlation Stroop RT vs Error")
 cor(full_stroop$stroop_rt, full_stroop$stroop_error)
+# The negative correlation means that participants who responded more slowly 
+# tended to make fewer errors. In other words, there is a speed¨Caccuracy trade-off.
